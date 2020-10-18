7801b3ad023c57c51bc02af6ea60f59a03dc0f23
Johnny Heredia <johnny@protocoloweb.ec>
2020-10-18 07:56:19 +0000

    calculo cilindro

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
-  printf("Hello World\n");
+  float radio;
+  float altura;
+  float pi = 3.14;
+  float area;
+  float volumen;
+
+
+  printf("Vamos a calcular el area de un cilindro!\n");
+  
+
+  printf("Ingresa el radio de la base: ");
+  scanf("%f", &radio);
+
+  printf("Ingresa la altura del cilindro ");
+  scanf("%f", &altura);
+
+  area = 2 * pi * radio * (radio + altura);
+  volumen = pi * pow(radio,2) * altura;
+
+  printf("El área de la base es: %f \n ", area);
+
+  printf("El volumen del cilindro es: %f ", volumen);
+
+
   return 0;
 }

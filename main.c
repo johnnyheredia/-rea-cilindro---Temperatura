13e63c2e9a75d799494f3607b2e999edaac92ab1
Johnny Heredia <johnny@protocoloweb.ec>
2020-10-18 08:15:19 +0000

    temperatura

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <math.h>
 
 int main(void) {
+  
+  //Calculo area de cilindro
+
   float radio;
   float altura;
   float pi = 3.14;
@@ -15,7 +18,7 @@ int main(void) {
   printf("Ingresa el radio de la base: ");
   scanf("%f", &radio);
 
-  printf("Ingresa la altura del cilindro ");
+  printf("Ingresa la altura del cilindro: ");
   scanf("%f", &altura);
 
   area = 2 * pi * radio * (radio + altura);
@@ -23,7 +26,23 @@ int main(void) {
 
   printf("El área de la base es: %f \n ", area);
 
-  printf("El volumen del cilindro es: %f ", volumen);
+  printf("El volumen del cilindro es: %f \n", volumen);
+
+
+  //Calculo grados fahrenheit a celsius
+  float valor;
+  float temperatura;
+
+  printf("Vamos a transformar grados fahrenheit a celsius\n");
+
+  printf("Ingresa la temperatura en grados fahrenheit: ");
+  scanf("%f", &valor);
+
+  temperatura = (((valor - 32) * 5) / 9);
+
+  printf("La temperatura en grados celcius es: %f", temperatura);
+
+
 
 
   return 0;
